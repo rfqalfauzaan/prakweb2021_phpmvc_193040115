cb7c0ad60dd85775ef3b915e0c7a3a58c3aef475
rfqalfauzaan <alfauzaan.rifqi@gmail.com>
2021-10-01 10:34:50 +0700

    Vid 1 sampe 3
    persiapan routing dan controller

---
+++ app/controllers/About.php
@@ -0,0 +1,11 @@
+<?php 
+class About {
+    public function index($nama = 'Rifqi',$pekerjaan = 'Mahasiswa')   
+    {
+        echo"Halo,nama saya $nama ,saya adalah seorang $pekerjaan " ;
+    } 
+    public function page()
+    {
+        echo 'about/page' ;
+    }
+}

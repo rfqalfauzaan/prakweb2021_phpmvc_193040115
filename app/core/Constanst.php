f891ca926638bd895a7d5a273489e4a974c41c4b
rfqalfauzaan <alfauzaan.rifqi@gmail.com>
2021-10-01 17:21:32 +0700

    Video 6
    Assests

---
+++ app/core/Constanst.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL','http://localhost/phpmvc/prakweb2021_phpmvc_193040115/public/');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,6 @@
-
-    <h1>About Me</h1>
+ <div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpeg" alt="Rifqi Al fauzaan" width="200" class="rounded-circle shadow">
     <p>Hallo, nama saya <?= $data['nama'];?>, umur saya <?= $data['umur'];?> tahun. 
     Saya adalah seorang <?= $data['pekerjaan'];?></p>
+ </div>
